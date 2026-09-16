88e7e5cccb2dd66358072cc3b36012d90de15074
sumanshetty17 <sumansheety17@gmail.com>
2026-09-16 18:40:36 +0530

    Delete src directory

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Studio } from "@/components/studio/Studio";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <Studio />;
-}
